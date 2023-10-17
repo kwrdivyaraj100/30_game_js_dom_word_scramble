b1677c59532862d63cf4959c13a055f1be76da51
Divya R <kwrdivyaraj100@gmail.com>
2023-10-17 17:34:29 +0530

    Added JavaScript logic for checking and displaying word correctness

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const wordText = document.querySelector(".word"),
-hintText = document.querySelector(".hint span"),
-timeText = document.querySelector(".time b"),
-inputField = document.querySelector("input");
+    hintText = document.querySelector(".hint span"),
+    timeText = document.querySelector(".time b"),
+    inputField = document.querySelector("input"),
+    refreshBtn = document.querySelector(".refresh-word"),
+    checkBtn = document.querySelector(".check-word");
 
 
 
@@ -16,7 +18,7 @@ const initGame = () => {
         let j = Math.floor(Math.random() * (i + 1));
         [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
-    
+
     wordText.innerText = wordArray.join("");
     hintText.innerText = randomObj.hint;
     correctWord = randomObj.word.toLowerCase();;
@@ -24,9 +26,41 @@ const initGame = () => {
     inputField.setAttribute("maxlength", correctWord.length);
     scoreArea.innerHTML = score;
 
-    if(score > 9)
-    {
+    if (score > 9) {
         winGame();
     }
 
 }
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+
+    if (!userWord) {
+        modal.style.display = "block";
+        modalContent.classList.remove("modal-wrong");
+        modalContent.classList.remove("modal-correct");
+        return modalText.innerHTML = `<br>Please enter the word to check!`;
+    }
+
+    if (userWord !== correctWord) {
+        if (score >= 1) {
+            score = score - 1;
+            scoreArea.innerHTML = score;
+        }
+        modal.style.display = "block";
+        modalContent.classList.add("modal-wrong");
+        return modalText.innerHTML = `<br>Oops! <b>${userWord}</b> is not a correct word`;
+    }
+    else {
+        modal.style.display = "block";
+        modalContent.classList.remove("modal-wrong");
+        modalContent.classList.add("modal-correct");
+        modalText.innerHTML = `<br>Congrats! <b>${correctWord.toUpperCase()}</b> is the correct word`;
+        score++;
+    }
+
+    initGame();
+}
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
