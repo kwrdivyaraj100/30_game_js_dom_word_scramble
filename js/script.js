c9d4b5bcf0802c9d7ee5a98786adc447a2405d34
Divya R <kwrdivyaraj100@gmail.com>
2023-10-17 17:30:42 +0530

    Initializing the game

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input");
+
+
+
+
+
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+    scoreArea.innerHTML = score;
+
+    if(score > 9)
+    {
+        winGame();
+    }
+
+}
